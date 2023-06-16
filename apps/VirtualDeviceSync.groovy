97bd1654d91586d726c44f2767835a317275bfe6
scubamikejax904 <55805086+scubamikejax904@users.noreply.github.com>
2023-06-16 09:45:42 -0400

    Update VirtualDeviceSync.groovy

--- apps/VirtualDeviceSync.groovy
+++ apps/VirtualDeviceSync.groovy
@@ -13,8 +13,10 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
+//0.0.1MSG Modified to add Motion Sensors and made no required fields By Michael G
+//0.0.2MSG Added Locks
 
-public static String version() { return "0.0.1" }
+public static String version() { return "0.0.2MSG" }
 
 definition(
     name: "Virtual Device Sync",
@@ -35,12 +37,21 @@ preferences {
      page(name: "mainPage", title: "Virtual Device Sync", install: true, uninstall: true) {
         section("Contact Sensor") {
             paragraph "Virtual Device Sync  v${version()}"
-            input name: "physicalContactSensor", type: "capability.contactSensor", title: "When open/close is detected on...", submitOnChange: true, required: true, multiple: false
-            input name: "virtualContactSensor", type: "capability.contactSensor", title: "Then update state on this sensor ...", submitOnChange: true, required: true, multiple: false
+            input name: "physicalSensor", type: "capability.contactSensor", title: "When open/close is detected on...", submitOnChange: true, required: false, multiple: false
+            input name: "virtualSensor", type: "capability.contactSensor", title: "Then update state on this sensor ...", submitOnChange: true, required: false, multiple: false
         }
+        section("Motion Sensor") {
+            paragraph "Virtual Device Sync  v${version()}"
+            input name: "physicalmotionSensor", type: "capability.motionSensor", title: "When motion is detected on...", submitOnChange: true, required: false, multiple: false
+            input name: "virtualmotionSensor", type: "capability.motionSensor", title: "Then update state on this sensor ...", submitOnChange: true, required: false, multiple: false
+    }
+    section("Locks") {
+            paragraph "Virtual Device Sync  v${version()}"
+            input name: "physicallock", type: "capability.lock", title: "When Lock state is changed on...", submitOnChange: true, required: false, multiple: false
+            input name: "virtuallock", type: "capability.lock", title: "Then update state on this Lock ...", submitOnChange: true, required: false, multiple: false
     }
 }
-
+}    
 def installed() {
     log.info "installed()"
     log.debug "installed()"
@@ -50,8 +61,12 @@ def installed() {
 def updated() {
    log.info "updated()"
    unsubscribe()
-   app.updateLabel("${physicalContactSensor?.displayName} --> ${virtualContactSensor?.displayName} </span>")
-   subscribe(physicalContactSensor, "contact", contactHandler)
+   app.updateLabel("${physicalSensor?.displayName} --> ${virtualSensor?.displayName} </span>")
+    app.updateLabel("${physicalmotionSensor?.displayName} --> ${virtualmotionSensor?.displayName} </span>")
+    app.updateLabel("${physicallock?.displayName} --> ${virtuallock?.displayName} </span>")
+   subscribe(physicalSensor, "contact", contactHandler)
+   subscribe(physicalmotionSensor, "motion", motionHandler)
+   subscribe(physicallock, "lock", lockHandler)
 }
 
 def uninstalled() {
@@ -63,11 +78,35 @@ def contactHandler(evt) {
     log.info "contactHandler() called: ${evt.descriptionText} ${evt.device?.name}"
     log.debug "contactHandler() called: ${evt.name} ${evt.value}"
     if(evt.value == "open"){
-        log.info "Sending open() to ${virtualContactSensor.name}"
-        virtualContactSensor.open()
+        log.info "Sending open() to ${virtualSensor.name}"
+        virtualSensor.open()
     }
     else {
-        log.info "Sending close() to ${virtualContactSensor.name}"
-        virtualContactSensor.close()
+        log.info "Sending close() to ${virtualSensor.name}"
+        virtualSensor.close()
     }
 }
+    def motionHandler(evt) {
+    log.info "motionHandler() called: ${evt.descriptionText} ${evt.device?.name}"
+    log.debug "motionHandler() called: ${evt.name} ${evt.value}"
+    if(evt.value == "active"){
+        log.info "Sending active() to ${virtualmotionSensor.name}"
+        virtualmotionSensor.active()
+    }
+    else {
+        log.info "Sending inactive() to ${virtualmotionSensor.name}"
+        virtualmotionSensor.inactive()
+    }
+    }
+        def lockHandler(evt) {
+    log.info "lockHandler() called: ${evt.descriptionText} ${evt.device?.name}"
+    log.debug "lockHandler() called: ${evt.name} ${evt.value}"
+    if(evt.value == "locked"){
+        log.info "Sending lock() to ${virtuallock.name}"
+        virtuallock.lock()
+    }
+     else {
+        log.info "Sending unlock() to ${virtuallock.name}"
+       virtuallock.unlock()
+    }
+        }
